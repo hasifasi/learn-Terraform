7467d2228b6e0f069dc083a947a9c39f63e45058
Mohammed <i23281@verisk.com>
2024-10-25 21:14:01 -0400

    Varraible in TF

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -65,3 +65,9 @@ output "map1_aws_trainer" {
   value = var.map1["aws"]["trainer"]
   #value = var.map1["aws"].trainer
 }
+
+# Declare a empty variable and we can get the value from CLI, Unlike ansible or shell we must and should declare a variable in order to use it.
+variable "trainer_name" {}
+output "trainer" {
+  value = var.trainer_name
+}
