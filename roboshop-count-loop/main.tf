d09cdc95dea63dd9a83d011b7a5e292117fe01eb
Mohammed <i23281@verisk.com>
2024-10-26 13:23:03 -0400

    rs-for-each

--- roboshop-count-loop/main.tf
+++ roboshop-count-loop/main.tf
@@ -19,7 +19,9 @@ resource "aws_instance" "inst" {
   count=length(var.instances)
   ami="ami-09c813fb71547fc4f"
   instance_type="t3.small"
+  subnet_id  = "subnet-064c2c91998fc5ef9"
   vpc_security_group_ids=["sg-01024740791e247b2"]
+  associate_public_ip_address = true
   tags={
     Name=var.instances[count.index]
   }
