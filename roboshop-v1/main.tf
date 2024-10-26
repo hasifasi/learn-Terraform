b970e256fc8323c675cf86440d7ff4c4cc7175c1
Mohammed <i23281@verisk.com>
2024-10-25 20:19:50 -0400

    create VPC

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
     ami="ami-09c813fb71547fc4f"
     instance_type="t3.small"
-    vpc_security_group_ids=["sg-0b8d3521b37542fe5"]
+    vpc_security_group_ids=["sg-095c9d6609b27bb7b"]
     tags={
         Name="frontend"
         }
@@ -10,7 +10,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "catalogue" {
     ami="ami-09c813fb71547fc4f"
     instance_type="t3.small"
-    vpc_security_group_ids=["sg-0b8d3521b37542fe5"]
+    vpc_security_group_ids=["sg-095c9d6609b27bb7b"]
     tags={
         Name="frontend"
         }
