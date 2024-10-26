de7562848026eba345806b04edb79bd7ba67c723
Mohammed <i23281@verisk.com>
2024-10-26 13:29:23 -0400

    rs-for-each

--- roboshop-for-each/main.tf
+++ roboshop-for-each/main.tf
@@ -18,7 +18,9 @@ resource "aws_instance" "inst" {
   for_each = var.instances
   ami="ami-09c813fb71547fc4f"
   instance_type="t3.small"
+  subnet_id = "subnet-064c2c91998fc5ef9"
   vpc_security_group_ids=["sg-01024740791e247b2"]
+  associate_public_ip_address = true
   tags={
     Name=each.key
   }
