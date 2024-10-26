92cd9e49ad156d9a257f42d264c1558c464f2fa0
Mohammed <i23281@verisk.com>
2024-10-25 21:53:12 -0400

    count-Loops in TF

---
+++ roboshop-count-loop/main.tf
@@ -0,0 +1,35 @@
+variable "instances" {
+  default = [
+    "frontend",
+    "catalogue",
+    "mongodb",
+    "cart",
+    "user",
+    "shipping",
+    "payment",
+    "mysql",
+    "redi",
+    "rabbitmq"
+
+  ]
+}
+
+
+resource "aws_instance" "inst" {
+  count=length(var.instances)
+  ami="ami-09c813fb71547fc4f"
+  instance_type="t3.small"
+  vpc_security_group_ids=["sg-095c9d6609b27bb7b"]
+  tags={
+    Name=var.instances[count.index]
+  }
+}
+
+resource "aws_route53_record" "record" {
+  count= length(var.instances)
+  zone_id = "Z048101918SLM3R11OGT3"
+  name    = "${var.instances[count.index]}-dev.waferhassan.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.inst[count.index].private_ip]
+}
