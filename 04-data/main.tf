5deddae8dc3560188abb57aff45840124969fa49
Mohammed <i23281@verisk.com>
2024-10-26 20:03:36 -0400

    data source

--- 04-data/main.tf
+++ 04-data/main.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "test" {
   ami= data.aws_ami.example.id
   instance_type="t3.small"
+  subnet_id = "subnet-012206b4eaf203149"
   vpc_security_group_ids=[data.aws_security_group.sg.id]
+  associate_public_ip_address = true
   tags={
     Name="test"
   }
