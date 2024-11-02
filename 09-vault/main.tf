9fdaa17f22e70e7728d4500f2f9e6a24333d659e
Mohammed <i23281@verisk.com>
2024-11-01 20:09:32 -0400

    Anisible Vault

---
+++ 09-vault/main.tf
@@ -0,0 +1,20 @@
+provider "vault" {
+  address = "https://172.31.39.97:8200"
+  token=var.vault_token
+  skip_tls_verify = false
+}
+
+variable "vault_token" {}
+
+
+data "vault_generic_secret" "example_creds" {
+  path = "test/my_credentials"
+}
+
+data "template_file" "example_template" {
+  template = file("./example.tmpl")
+  vars = {
+    username = data.vault_generic_secret.example_creds.data["username"]
+    password = data.vault_generic_secret.example_creds.data["password"]
+  }
+}
